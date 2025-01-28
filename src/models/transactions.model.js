d948c420e4baf66e62413a965bc61c57013415ff
Phathy <phathy.nap@gmail.com>
2025-01-28 19:37:55 +0700

    change th tag to td tag in index.ejs and format the data to be shorter

--- src/models/transactions.model.js
+++ src/models/transactions.model.js
@@ -3,14 +3,22 @@ import pool from "../config/database.js";
 class TransactionsModel {
   static async getAll() {
     try {
-      const { rows } = await pool.query("SELECT * FROM all_transactions");
+      const query = `
+        SELECT 
+          id, 
+          product_name, 
+          TO_CHAR(date, 'YYYY-MM-DD') AS date, 
+          qty, 
+          total_price 
+        FROM 
+          all_transactions
+      `;
+      const { rows } = await pool.query(query);
       return rows;
     } catch (error) {
       throw new Error(`Can't fetch transactions data ${error.message}`);
     }
   }
-
 }
-
 
 export default TransactionsModel;
